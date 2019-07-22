eb58d09e66dd56cd858e981962d417ebdfdab396
Natalia <nat.mietus@gmail.com>
2019-07-22 22:36:24 +0100

    Standalone function for random quotation part generation
    s

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
 
 
-// quotation object end
 
 // quote base
-    let quoteBeginning =[ "Focus on", "Do not wait for", "Demonstrate", "Do not judge", "Be confident with", "Keep an eye on"]
 
-    let quoteMiddle =[ "your health", "the unbearable lightness of dawn sleeping bag", "serious trouble", "your possesion", "awareness", "your observation"]
+class quoteBase {
+    constructor (quotePart){
+        this.quotePart = quotePart;
+    }
 
-    let quoteEnding =[ "and let it be your indicator", "- it may bypass you", "- it can pay off in peculiar ways", "and it will become your habit eventually", "and stand firmly by your convictions","and a dream you have will come true"]
+    generateRandomQuotePart() {
+     let randomSentencePart = this.quotePart[Math.floor(Math.random()*this.quotePart.length)];
+      return randomSentencePart;
+    }
+};
 
 
-// variables
+let quoteBeginning = new quoteBase (
+    [ "Focus on", "Do not wait for", "Demonstrate", "Do not judge", "Be confident with", "Keep an eye on"]
+ );
+
+ let quoteMiddle = new quoteBase (
+    [ "your health", "the unbearable lightness of a dawn sleeping bag", "serious trouble", "your possesion", "awareness", "your observation"]
+ );
+
+ let quoteEnding = new quoteBase (
+    [ "and let it be your indicator", "- it may bypass you", "- it can pay off in peculiar ways", "and it will become your habit eventually", "and stand firmly by your convictions","and a dream you have will come true"]
+ );
+
+
+// constants
 const btn = document.getElementById("btn");
 const quote = document.getElementById("quote");
 
 
+
+
 //event listener
+btn.addEventListener('click', createNewQuotation);
 
-btn.addEventListener('click', getQuote);
 
-function getQuote () {
-    let number = Math.floor(Math.random() * quoteBeginning.length);
-    let qmiddle = Math.floor(Math.random() * quoteMiddle.length);
-    let qending = Math.floor(Math.random() * quoteEnding.length);
+function createNewQuotation () {
 
-    quote.innerHTML = '<span>"</span>' + quoteBeginning[number] + ' ' + quoteMiddle[qmiddle] +' ' + quoteEnding[qending] + '.'+'<span>"</span>';
+    quote.innerHTML = '<span>"</span>' + quoteBeginning.generateRandomQuotePart() + ' ' + quoteMiddle.generateRandomQuotePart() +' ' + quoteEnding.generateRandomQuotePart() + '.'+'<span>"</span>';
     
 }
 
-let x = 5;
-x = true;
