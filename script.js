2ea1fa58059ff775cc89c66a7c5724fef2f0e6de
Natalia <nat.mietus@gmail.com>
2019-08-08 16:12:46 +0100

    Changed event targets to be grabbed by id instead of class name

--- script.js
+++ script.js
@@ -26,19 +26,19 @@ const quote = document.getElementById("quote");
 let btnClearAll = document.getElementById("btnClearAll")
 let chosenNumberOfQuotes = document.getElementById("chosenNumberOfQuotes");
 
-// click event delegation  // TODO: grab by id instead of class?
+// click event delegation 
 const parent = document.querySelector("body");
 
 parent.addEventListener('click', event => {
-	if (event.target.className === 'btnFortune') {
+	if (event.target.id === 'btnFortune') {
         generateQuotes(quoteBeginning, quoteMiddle, quoteEnding);
     }
 
-    if (event.target.className === 'btnMisfortune') {
+    if (event.target.id === 'btnMisfortune') {
         generateQuotes(quoteBeginning, quoteMiddle, misfortuneEnding);
     }
 
-    if (event.target.className === 'btnClearAll') {
+    if (event.target.id === 'btnClearAll') {
         clearAll();
     }
  });
@@ -51,17 +51,17 @@ function clearAll() {
     while (child) { 
         ul.removeChild(child); 
         child = ul.lastElementChild; 
-        btnClearAll.style.display = "none";
+        btnClearAll.style.display = "none"; // hides "Clear all" button that is generated along with the random quote list
     } 
 } 
 
- // randomQuotePart function
+ // randomQuotePart function - choses random element from quote part array
  function randomQuotePart(quotePartArray) {
     let randomNumber = (Math.floor(Math.random() * quotePartArray.quotePart.length));
     return quotePartArray.quotePart[randomNumber];
  };
 
-// generate quote type & loop  // TODO: quote type parameter?
+// generate quote type & loop 
 function generateQuotes(cookieBeginning, cookieMiddle, cookieEnding){
     clearAll();
 for ( let i = 0; i < chosenNumberOfQuotes.value; i++) {
